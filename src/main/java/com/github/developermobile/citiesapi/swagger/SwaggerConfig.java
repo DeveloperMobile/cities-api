c542c058848d1ab59f77bc50283ec2bcfbc46123
DeveloperMobile <tiagopereiravieira@gmail.com>
2022-05-23 21:18:32 -0300

    update SwaggerConfig class

--- src/main/java/com/github/developermobile/citiesapi/swagger/SwaggerConfig.java
+++ src/main/java/com/github/developermobile/citiesapi/swagger/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.github.developermobile.citiesapi.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -34,5 +36,14 @@ public class SwaggerConfig {
                 .version("1.0")
                 .build();
 
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
